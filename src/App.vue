a49e286c8340c5a6d39766711df74465fb37d69c
Eamon <xiaym18@midea.com>
2024-03-07 17:29:24 +0800

    feat: 配置代理跨域和对axios进行二次封装

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,16 @@
 </template>
 
 <script setup lang="ts">
-
+import request from "@/utils/request";
+import { onMounted } from "vue";
+onMounted(() => {
+  request
+    .get("/hosp/hospital/1/10")
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => console.log(err));
+});
 </script>
 
 <style scoped>
